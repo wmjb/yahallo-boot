4de701d3b24d319837ed4ac1e4c3f5e213d7cc70
wmjb <77227948+wmjb@users.noreply.github.com>
2023-04-30 22:20:45 +0100

    Update Devices.h

--- TegraSecureBootUnlock/Include/Devices.h
+++ TegraSecureBootUnlock/Include/Devices.h
@@ -13,10 +13,23 @@ typedef struct _SURFACE2_FW_OFFSET_INFO {
   UINT32 UefiSecureWorldSecureBootStringOffset;
 } SURFACE2_FW_OFFSET_INFO, *PSURFACE2_FW_OFFSET_INFO;
 
+typedef struct _TEGRA3_OFFSET_INFO {
+  // Non secure world offsets
+  UINT32 UefiNsWorldBufferStartOffset;
+  UINT32 UefiNsWorldBufferEndOffset;
+  // Secure world offsets
+  UINT32 UefiSecureWorldRuntimeFlag1Offset;
+  UINT32 UefiSecureWorldRuntimeFlag2Offset;
+  UINT32 UefiSecureWorldSetupModeStringOffset;
+  UINT32 UefiSecureWorldSecureBootStringOffset;
+} TEGRA3_OFFSET_INFO, *PTEGRA3_OFFSET_INFO;
+
 VOID SurfaceRTExploit(VOID);
 VOID Surface2Exploit(PSURFACE2_FW_OFFSET_INFO pFwInfo);
+VOID tegra3exploit(PTEGRA3_OFFSET_INFO pFwInfo);
 
 VOID Surface2_Fw_2_06_500_Exploit(VOID);
 VOID Surface2_Fw_4_22_500_Exploit(VOID);
-
+VOID Yoga_11_Exploit(VOID);
+VOID VivoTab_Exploit(VOID);
 #endif
